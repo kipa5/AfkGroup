41bb56810d56fded0597043a52542865577b77a0
kipa5 <farisrydnr@gmail.com>
2025-06-21 14:56:41 +0800

    Menambahkan tiket ke dalam lingked list

--- AfkGroup.cpp
+++ AfkGroup.cpp
@@ -22,3 +22,16 @@ struct Tiket {
     int hargaPerTiket;
     Tiket* next;
 };
+ // Menambahkan tiket baru ke dalam linked list
+void tambahTiket(Tiket*& head, string nama, string judul, int jumlah, int harga) {
+    Tiket* baru = new Tiket{nama, judul, jumlah, harga, nullptr};
+    if (head == nullptr) {
+        head = baru;
+    } else {
+        Tiket* temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = baru;
+    }
+    cout << "\nTiket berhasil ditambahkan atas nama " << nama << endl;
+}
